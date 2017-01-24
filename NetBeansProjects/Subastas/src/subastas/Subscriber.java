003f73de441f99842a5abd86bd3379c9ba381a18
Stephany Quimba <step-quim@hotmail.com>
2017-01-23 20:18:41 -0500

    inicializar clase subscriber

--- NetBeansProjects/Subastas/src/subastas/Subscriber.java
+++ NetBeansProjects/Subastas/src/subastas/Subscriber.java
@@ -1,73 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package subastas;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
- 
+import javax.jms.Topic;
+
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
- 
-/**
- *
- * @author henry
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Subscriber {
-    private ConnectionFactory connectionFactory;
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(Subscriber.class);
+
+    private static final String NO_GREETING = "no greeting";
+
+    private String clientId;
     private Connection connection;
     private Session session;
-    private javax.jms.MessageConsumer messageConsumer;
- 
-    public void consumeMessage() {
-	try {
-			// get the ConnectionFactory
-			// default broker URL is tcp://localhost:61616
-			connectionFactory = new ActiveMQConnectionFactory(
-					ActiveMQConnection.DEFAULT_BROKER_URL);
-			// create a Connection object
-			connection = connectionFactory.createConnection();
-			// start the connection
-			connection.start();
-			// create a Session object
-			// transaction false
-			// auto acknowledgment sent when sending or receiving a message
-			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			// create a destination - Queue or Topic - where message will be
-			// sent or received from
-			Destination destination = session
-					.createTopic("topic");
-			// create a Producer who will send the message
-			messageConsumer = session.createConsumer(destination);
-			// consume message sent by Producer
-			Message message = messageConsumer.receive();
-			// check whether it is an instance of TextMessage
-			// because we have let Producer send TextMessage
-			// there may be other message type
-			if (message instanceof TextMessage) {
-				TextMessage textMessage = (TextMessage) message;
-				System.out.println("Got message published by Publisher : "
-						+ textMessage);
-			} else {
-				throw new JMSException("Message must be a type of TextMessage");
-			}
-		} catch (JMSException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				messageConsumer.close();
-				session.close();
-				connection.close();
-			} catch (JMSException e) {
-				e.printStackTrace();
-			}
-		}
-            }
-    
+    private MessageConsumer messageConsumer;
+
+    public void create(String clientId, String topicName) throws JMSException {
+        this.clientId = clientId;
+
+        // create a Connection Factory
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                ActiveMQConnection.DEFAULT_BROKER_URL);
+
+        // create a Connection
+        connection = connectionFactory.createConnection();
+        connection.setClientID(clientId);
+
+        // create a Session
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // create the Topic from which messages will be received
+        Topic topic = session.createTopic(topicName);
+
+        // create a MessageConsumer for receiving messages
+        messageConsumer = session.createConsumer(topic);
+
+        // start the connection in order to receive messages
+        connection.start();
+    }
 }
