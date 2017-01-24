3d543d9ef04743cdd008e2863764bad47a9c5623
henryxavier10 <henryxavier10@gmail.com>
2017-01-23 23:05:05 -0500

    arreglando errores

---
+++ Subastas/src/subastas/Subastador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subastas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author henry
+ */
+public class Subastador {
+    public String nombre;
+    public ArrayList<Producto>productos=new ArrayList();
+
+    public Subastador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+    
+}
---
+++ Subastas_participante/src/subastas_participante/Participante.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subastas;
+
+/**
+ *
+ * @author henry
+ */
+public class Participante {
+    public String nombre;
+    public double valor_oferta;
+    public double capital;
+    
+    
+    public Participante(String nombre, double valor_oferta, double capital) {
+        this.nombre = nombre;
+        this.valor_oferta = valor_oferta;
+        this.capital = capital;
+    }
+
+    public double getCapital() {
+        return capital;
+    }
+
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
+
+  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getValor_oferta() {
+        return valor_oferta;
+    }
+
+    public void setValor_oferta(double valor_oferta) {
+        this.valor_oferta = valor_oferta;
+    }
+    
+}
