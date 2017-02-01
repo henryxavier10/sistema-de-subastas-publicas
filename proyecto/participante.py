f19eaf317efa0c47cfb3097fa1d29b6adc1d0d03
sianna93 <step-quim@hotmail.com>
2017-02-01 01:25:36 -0500

    agregando ver subasta

--- proyecto/participante.py
+++ proyecto/participante.py
@@ -64,65 +64,20 @@ def crear_subasta(nombre_subastador):
 	   		canal="subastas"
 	   		mensaje="Subasta "+canal_subasta+"creada por"+nombre_subastador+"esta abierta"
 	   		r.publish(canal, mensaje)
-	   		ver_subastas(canal, mensaje,canal_subasta,nombre_subastador)
+	   		ver_subastas(canal, mensaje,canal_subasta)
 
-def ver_subastas(canal,mensaje,canal_subasta,nombre_subastador):
+def ver_subastas(canal,mensaje,canal_subasta):
 	print ("*************************************************************")
 	print ("*************************************************************")
 	print ("***********************MI SUBASTA****************************")
 	print ("*************************"+canal_subasta+"*****************")
 	print ("*************************************************************")
-	print ("Ingrese <terminar> para dar por terminada la subasta")
 	
-	opcion =True
-	while opcion:
-		producto=raw_input("Ingrese descripcion del producto\n")
-		precio=raw_input("Ingrese precio del producto\n")
-		
-		if producto =="salir" or precio =="salir":
-			menu_principal(nombre_subastador)
-		else:
-			if producto=="" or precio=="":
-				print("\n Ingrese informacion valida")
-			else:
-		   		canal=canal_subasta
-		   		mensaje="Procducto: "+producto+"\nPrecio: "+precio
-		   		r.publish(canal, mensaje)
-
-		   		pubsub = r.pubsub()
-				pubsub.subscribe("canal_subasta")
-				while True:
-					for item in pubsub.listen():
-						print item['data']
-
-def ver_ofertas(canal,mensaje,canal_subasta,nombre_subastador):
-	print ("*************************************************************")
-	print ("*************************************************************")
-	print ("***********************ESPERANDO PUJAS***********************")
-	print ("*************************"+canal_subasta+"*****************")
-	print ("*************************************************************")
-	print ("Ingrese <salir> para regresar")
-	
-	opcion =True
-	while opcion:
-		producto=raw_input("Ingrese descripcion del producto\n")
-		precio=raw_input("Ingrese precio del producto\n")
-		
-		if producto =="salir" or precio =="salir":
-			menu_principal(nombre_subastador)
-		else:
-			if producto=="" or precio=="":
-				print("\n Ingrese informacion valida")
-			else:
-		   		canal=canal_subasta
-		   		mensaje="Procducto: "+producto+"\nPrecio: "+precio
-		   		r.publish(canal, mensaje)
-
-		   		pubsub = r.pubsub()
-				pubsub.subscribe("canal_subasta")
-				while True:
-					for item in pubsub.listen():
-						print item['data']
+	pubsub = r.pubsub()
+	pubsub.subscribe("canal_subasta")
+	while True:
+		for item in pubsub.listen():
+			print item['data']
 
 
 
